b10b06323ea9da4bef6743fa5edd9708cb35bae6
Rob Garner <rgarner011235@gmail.com>
2020-11-30 18:52:27 -0700

    Add project files.

---
+++ PaypalDemo/Client/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿function render_paypal_button(total) {
+    paypal.Button.render({
+        // Configure environment
+        env: 'sandbox',
+        client: {
+            sandbox: 'AcI22M05nHrP-7fWLDrIJcG9N5XG04IImnUr3j2U7SAP0n9U3Bk0rjIp4EZRZ_quybEYp2aXXjxfpvq9'
+            //,production: 'demo_production_client_id'
+        },
+        // Customize button (optional)
+        locale: 'en_US',
+        style: {
+            size: 'small',
+            color: 'gold',
+            shape: 'pill',
+        },
+
+        // Enable Pay Now checkout flow (optional)
+        commit: true,
+
+        // Set up a payment
+        payment: function (data, actions) {
+            return actions.payment.create({
+                transactions: [{
+                    amount: {
+                        total: total,
+                        currency: 'USD'
+                    }
+                }]
+            });
+        },
+        // Execute the payment
+        onAuthorize: function (data, actions) {
+            return actions.payment.execute().then(function () {
+                // Show a confirmation message to the buyer
+                window.alert('Thank you for your purchase!');
+            });
+        }
+    }, '#paypal-button');
+}
